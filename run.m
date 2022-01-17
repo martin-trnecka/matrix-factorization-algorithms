6d0d5b16efae0be7dafb900d155781382c57115f
martin-trnecka <42838144+martin-trnecka@users.noreply.github.com>
2022-01-17 17:51:24 +0100

    Update run.m
    added an example of GreEss usage

--- run.m
+++ run.m
@@ -1,8 +1,17 @@
 % example of usage
 load mushroom.mat
 
+% GreConD algorithm
 tic();
 [A,B] = GreConD(mushroom);
 toc();
 
-I = bprod(A,B); 
+I = bprod(A,B);
+
+
+% GreEss algorithm
+tic();
+[C,D] = GreConD(mushroom);
+toc();
+
+I = bprod(C,D);
